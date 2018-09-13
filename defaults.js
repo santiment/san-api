eed9a7a373c0d56e71bc7fe93354643d9b2ca38b
DmitriVanGuard <dimavanguard@gmail.com>
2018-09-13 16:42:24 +0300

    Utils module. Express removed

---
+++ defaults.js
@@ -0,0 +1,50 @@
+export const examples = [
+  {
+    name: 'dailyActiveAddresses',
+    title: 'Daily Active Addresses'
+  },
+  {
+    name: 'burnRate',
+    title: 'Token aging (burn rate)'
+  },
+  {
+    name: 'transactionVolume',
+    title: 'Transaction volume'
+  },
+  {
+    name: 'exchangeFundsFlow',
+    title: 'Exchange Funds Flow'
+  },
+  {
+    name: 'githubActivity',
+    title: 'Github Activity',
+    skipArgs: ['movingAverageIntervalBase', 'ticker', 'to', 'transform'],
+    notes: `\n\n\n[An article explaining our approach to tracking github activity](https://medium.com/santiment/tracking-github-activity-of-crypto-projects-introducing-a-better-approach-9fb1af3f1c32){:target="_blank"}`
+  },
+  {
+    name: 'erc20ExchangeFundsFlow',
+    title: 'ERC20 Exchange Funds Flow'
+  },
+  {
+    name: 'socialVolume',
+    title: 'Social Volume'
+  },
+  {
+    name: 'socialVolumeProjects',
+    title: 'Social Volume Projects'
+  },
+  {
+    name: 'topicSearch',
+    title: 'Topic search'
+  }
+]
+
+export const argumentDefaults = {
+  slug: `"dragonchain"`,
+  from: `"2018-08-01 16:00:00Z"`,
+  to: `"2018-08-05 16:00:00Z"`,
+  interval: `"1d"`,
+  source: 'TELEGRAM',
+  searchText: `"btc moon"`,
+  socialVolumeType: 'TELEGRAM_DISCUSSION_OVERVIEW'
+}
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,23 @@
-import App, { Container } from 'next/app'
-
 import Head from 'next/head'
 import Docs from '../components/Docs'
-import { graphql, ApolloProvider } from 'react-apollo'
+import { ApolloProvider } from 'react-apollo'
 import ApolloClient from 'apollo-client'
 import errorLink from '../components/apollo/error-link'
 import retryLink from '../components/apollo/retry-link'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { from } from 'apollo-link'
 import { createHttpLink } from 'apollo-link-http'
+import fetch from 'isomorphic-fetch'
 
 const httpLink = createHttpLink({
-  uri: 'https://api-stage.santiment.net/graphql'
+  uri: 'https://api-stage.santiment.net/graphql',
+  fetch
 })
 
 const client = new ApolloClient({
   link: from([httpLink, errorLink, retryLink]),
   cache: new InMemoryCache()
 })
-
-const host = process.env.NODE_ENV === 'development'
-  ? 'localhost:3000'
-  : 'api.santiment.net'
 
 const IndexPage = props => {
   console.log('TCL: props', props)
