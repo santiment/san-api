d7cdb62cf067d369d67c51249522ba6ff3950acd
DmitriVanGuard <dimavanguard@gmail.com>
2018-11-23 12:08:42 +0300

    REQUIRED_SAN_STAKE_FULL_ACCESS env var

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   publicRuntimeConfig: {
-    backendUrl: process.env.BACKEND_URL
+    backendUrl: process.env.BACKEND_URL,
+    requiredSanStakeFullAccess: process.env.REQUIRED_SAN_STAKE_FULL_ACCESS
   }
 }
