00442eabe5816084de6a2726e47d52809368360d
DmitriVanGuard <dimavanguard@gmail.com>
2018-12-20 16:33:15 +0300

    CSS loader. Importing css to index page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+const withCSS = require('@zeit/next-css')
+
+module.exports = withCSS({
   publicRuntimeConfig: {
     backendUrl: process.env.BACKEND_URL,
     requiredSanStakeFullAccess: process.env.REQUIRED_SAN_STAKE_FULL_ACCESS
   }
-}
+})
--- server.js
+++ server.js
@@ -8,8 +8,6 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
     const parsedUrl = parse(req.url, true)
     const { pathname, query } = parsedUrl
 
@@ -19,29 +17,3 @@ app.prepare().then(() => {
     console.log('> Ready on http://localhost:3000')
   })
 })
-
-// const PORT = 8000
-
-// app.prepare().then(() => {
-//   const server = express()
-
-//   const httpServer = http.createServer(function (req, res) {
-//     file.serve(req, res)
-//   })
-
-//   server.use(
-//     vhost('app.localhost', function (req, res) {
-//       httpServer.emit('request', req, res)
-//     })
-//   )
-//   server.use(
-//     vhost('api.localhost', (req, res, next) => {
-//       return handle(req, res, parse(req.url, true))
-//     })
-//   )
-
-//   server.listen(PORT, err => {
-//     if (err) throw err
-//     console.log(`Server ready on http://api.localhost:${PORT}`)
-//   })
-// })
